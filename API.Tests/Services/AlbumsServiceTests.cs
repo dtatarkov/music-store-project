4e3fda7683e6883aec624f098637b89a4805408c
Dmitriy Tatarkov <d.tatarkov@gmail.com>
2022-08-23 00:22:09 +0300

    api tests: added albums service tests

---
+++ API.Tests/Services/AlbumsServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using API.Context;
+using API.Entities;
+using API.Services;
+using MockQueryable.Moq;
+using Moq;
+
+namespace API.Tests.Services
+{
+    public class AlbumsServiceTests
+    {
+        [Fact]
+        public void GetAlbums()
+        {
+            var data = new List<Album>
+            {
+                new Album { AlbumId = 1 },
+                new Album { AlbumId = 2 },
+                new Album { AlbumId = 3 },
+            }.AsQueryable();
+
+            var mockSet = data.BuildMockDbSet();
+
+            var mockContext = new Mock<IApplicationContext>();
+            mockContext.Setup(c => c.Albums).Returns(mockSet.Object);
+
+            var service = new AlbumsService(mockContext.Object);
+            var blogs = service.GetAlbums().ToArray();
+
+            Assert.Equal(3, blogs.Length);
+            Assert.Equal(1, blogs[0].AlbumId);
+            Assert.Equal(2, blogs[1].AlbumId);
+            Assert.Equal(3, blogs[2].AlbumId);
+        }
+    }
+}
