5f11ec80c89dfbad0c84320f989726a5c742bc40
Dmitriy Tatarkov <d.tatarkov@gmail.com>
2022-08-22 16:03:59 +0300

    api: added albums controller

---
+++ API/Controllers/AlbumsController.cs
@@ -0,0 +1,25 @@
+﻿using API.DTO;
+using API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AlbumsController : ControllerBase
+    {
+        private readonly IAlbumsService albumsService;
+
+        public AlbumsController(IAlbumsService albumsService)
+        {
+            this.albumsService = albumsService;
+        }
+
+        [HttpGet]
+        public IQueryable<AlbumDTO> Get()
+        {
+            throw new NotImplementedException();
+            //return albumsService.GetAlbums();
+        }
+    }
+}
---
+++ API/Services/AlbumsService.cs
@@ -0,0 +1,20 @@
+﻿using API.Context;
+using API.Entities;
+
+namespace API.Services
+{
+    public class AlbumsService: IAlbumsService
+    {
+        private readonly ApplicationContext dbContext;
+
+        public AlbumsService(ApplicationContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IQueryable<Album> GetAlbums()
+        {
+            return dbContext.Albums.AsQueryable();
+        }
+    }
+}
