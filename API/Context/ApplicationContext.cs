a4836c415486d5b7bd469c3a082f4b22ea07e1fb
Dmitriy Tatarkov <d.tatarkov@gmail.com>
2022-08-22 16:03:07 +0300

    api: added dbcontext

---
+++ API/Context/ApplicationContext.cs
@@ -0,0 +1,31 @@
+﻿using API.Entities;
+using API.Settings;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+
+namespace API.Context
+{
+    public class ApplicationContext: DbContext
+    {
+        private readonly AppSettings appSettings;
+
+        public DbSet<Album> Albums { get; set; } = null!;
+
+        public ApplicationContext(AppSettings appSettings)
+        {
+            this.appSettings = appSettings;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            var psqlBuilder = new NpgsqlConnectionStringBuilder(appSettings.DB.ConnectionString)
+            {
+                Password = appSettings.DB.Password
+            };
+
+            optionsBuilder.UseNpgsql(psqlBuilder.ConnectionString);
+        }
+    }
+}
---
+++ API/Entities/Album.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class Album
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long AlbumId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTimeOffset CreatedDate { get; set; } = DateTimeOffset.Now;
+        public DateTimeOffset UpdatedDate { get; set; } = DateTimeOffset.Now;
+    }
+}
