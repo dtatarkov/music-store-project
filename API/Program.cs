5786b412e271277554ced5b6859ad3ec2669ae4b
Dmitriy Tatarkov <d.tatarkov@gmail.com>
2022-09-27 16:17:28 +0300

    api: added db to container

--- API/Program.cs
+++ API/Program.cs
@@ -5,39 +5,42 @@ using API.Validators;
 using NLog;
 using NLog.Web;
 
-var builder = WebApplication.CreateBuilder(args);
-
-GlobalDiagnosticsContext.Set("connectionString", builder.Configuration.GetValue<string>("LogsDB:ConnectionString"));
-
-builder.Logging.ClearProviders();
-builder.Host.UseNLog();
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer(); // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<IApplicationContext, ApplicationContext>();
-
-builder.Services.AddSingleton(services => new AppSettings(services.GetRequiredService<IConfiguration>()));
-builder.Services.AddSingleton<IAlbumValidator, AlbumValidator>();
-
-builder.Services.AddScoped<IAlbumsService, AlbumsService>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        GlobalDiagnosticsContext.Set("connectionString", builder.Configuration.GetValue<string>("LogsDB:ConnectionString"));
+        
+        builder.Logging.ClearProviders();
+        builder.Host.UseNLog();
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer(); // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddDbContext<IApplicationContext, ApplicationContext>();
+
+        builder.Services.AddSingleton(services => new AppSettings(services.GetRequiredService<IConfiguration>()));
+        builder.Services.AddSingleton<IAlbumValidator, AlbumValidator>();
+
+        builder.Services.AddScoped<IAlbumsService, AlbumsService>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
-
-//expose Program class so it can be access by integration tests
-public partial class Program { }
