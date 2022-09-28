7e66a8e05e9f044439201ca127f6e5aa010e5430
Dmitriy Tatarkov <d.tatarkov@gmail.com>
2022-09-28 23:57:53 +0300

    added client

---
+++ Client/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Client.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController() { }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
