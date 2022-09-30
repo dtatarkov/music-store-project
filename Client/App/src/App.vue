d96ab410db33ea20082fbff2de83240caa2df33a
Dmitriy Tatarkov <d.tatarkov@gmail.com>
2022-09-30 14:47:48 +0300

    client: header update

--- Client/App/src/App.vue
+++ Client/App/src/App.vue
@@ -1,85 +1,55 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+    import { RouterLink, RouterView } from 'vue-router';
+    import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <header class="app__header">
+        <div class="app__header__inner">
+            <div class="app__header__logo">
+                <i class="fa-regular fa-music"></i>
+            </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+            <nav class="app__header__navigation">
+                <RouterLink class="app__header__navigation__link" :to="{ name:'home' }">Home</RouterLink>
+                <RouterLink class="app__header__navigation__link" :to="{ name:'about' }">About</RouterLink>
+            </nav>
+        </div>
+    </header>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<style lang="scss" scoped>
+    .app__header {
+        display: flex;
+        justify-content: center;
+        background-color: var(--color-background-accent);
+        font-size: 1.2em;
+        color: var(--color-text-accent);        
+    }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+    .app__header__inner {
+        flex-grow: 1;
+        display: flex;
+        padding: var(--section-padding);
+        max-width: var(--viewport-size);
+        gap: var(--row-gap-big);
+    }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+    .app__header__navigation {
+        display: flex;
+        gap: var(--row-gap);
+    }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
+    .app__header__navigation__link {
+        color: var(--color-text-accent);
+        text-decoration: none;
+        text-shadow: 0 1px var(--color-text-shadow);
+        font-weight: 500;
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
+        &.router-link-active {
+            color: var(--color-text-accent-active);
+        }
+    }
 </style>
--- Client/App/vite.config.ts
+++ Client/App/vite.config.ts
@@ -11,7 +11,8 @@ export default defineConfig({
     plugins: [vue(), vueJsx()],
     resolve: {
         alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            '@fonts': fileURLToPath(new URL('./fonts', import.meta.url))
         }
     },
 
